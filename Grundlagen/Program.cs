e0d5bae32d847d28d330020a394be23f05e8ee1b
Ilkan Celik <ilkan.c2@gso.schule.koeln>
2022-09-08 10:09:21 +0200

    Projektdateien hinzufügen.

---
+++ Grundlagen/Program.cs
@@ -0,0 +1,38 @@
+﻿
+Console.WriteLine("Aufgabe 1");
+
+
+Console.WriteLine(" .-.");
+Console.WriteLine("(o o)");
+Console.WriteLine("| O |");
+Console.WriteLine("|   |");
+Console.WriteLine("'~~~'");
+
+
+
+Console.WriteLine("Aufgabe 2");
+
+Console.WriteLine("Geben sie ihren Vornamen ein!");
+string Vorname = Console.ReadLine();
+Console.WriteLine("Geben sie ihren Nachnamen ein!");
+string Nachname = Console.ReadLine();
+Console.WriteLine("Geben sie ihr Alter ein!");
+string Alter = Console.ReadLine();
+Console.WriteLine("Geben sie ihre Stadt ein!");
+string Stadt = Console.ReadLine();
+Console.WriteLine("Geben sie ihren plz ein!");
+string plz = Console.ReadLine();
+Console.WriteLine("Geben sie ihre Klasse ein!");
+string Klasse = Console.ReadLine();
+
+Console.WriteLine($"{Vorname}");
+Console.WriteLine($"{Nachname}");
+Console.WriteLine($"{Alter}");
+Console.WriteLine($"{Stadt}");
+Console.WriteLine($"{plz}");
+Console.WriteLine($"{Klasse}");
+
+Console.WriteLine("Aufgabe  3");
+
+Console.WriteLine($"{Vorname} ist {Alter} Jahre alt und besucht die Klasse {Klasse} des Georg-Simon-Ohm Berufskolleg");
+
